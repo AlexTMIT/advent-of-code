b1575361a2aeb0c919a37a36c96a79ab3d8783e0
Alex <atmogens@gmail.com>
2023-12-08 15:49:56 +0100

    day 8!

--- 2023/puzzle-8/puzzle-81.py
+++ 2023/puzzle-8/puzzle-81.py
@@ -1,6 +1,6 @@
 steps = 0
 
-with open('sample-input.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     lines = file.readlines()
 
 instructions = list(lines[0].strip())
@@ -9,9 +9,26 @@ dictionary = {}
 for line in lines[1:]:
     if "=" in line:
         key, values = line.strip().split("=")
-        values = values.replace("(", "").replace(")", "").split(", ")
+        values = [v.strip() for v in values.replace("(", "").replace(")", "").split(",")]
         dictionary[key.strip()] = values
 
-print(instructions)
-print(dictionary)
+index = -1
+key = 'AAA'
+travelling = True
 
+while travelling:
+    index += 1
+
+    if (key == "ZZZ"):
+        travelling = False
+        break;
+    if (len(instructions) <= index):
+        index = 0
+    if (instructions[index] == "R"):
+        key = dictionary[key][1] 
+    else: 
+        key = dictionary[key][0] 
+    
+    steps += 1
+
+print(steps)
